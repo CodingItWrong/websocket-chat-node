af4784e0c847f4a9cb7e7bf0ac87f24dc4269c0c
Josh Justice <me@codingitwrong.com>
2021-08-22 06:57:31 -0400

    Make messaging consistent

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ wss.on('connection', ws => {
   ws.send('Connected to Node');
 
   ws.on('message', message => {
-    console.log('received: %s', message);
-    ws.send(`Echo back from server: ${message}`);
+    console.log('Node received: %s', message);
+    ws.send(`Response from Node: ${message}`);
   });
 });
